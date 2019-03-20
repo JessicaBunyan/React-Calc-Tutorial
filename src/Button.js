848ed8f3a31a55b8614f2f21ed9c0d48705e7195
Jessica Bunyan <tristan@bunyan.com>
2019-03-20 20:49:07 +0000

    made button a functional component

--- src/Button.js
+++ src/Button.js
@@ -1,16 +1,11 @@
 import React, { Component } from "react";
 
-class Button extends Component {
-  render() {
-    return (
-      <div
-        className="number button"
-        onClick={() => this.props.onClick(this.props.value)}
-      >
-        {this.props.value}
-      </div>
-    );
-  }
-}
+const Button = props => {
+  return (
+    <div className="number button" onClick={() => props.onClick(props.value)}>
+      {props.value}
+    </div>
+  );
+};
 
 export default Button;
